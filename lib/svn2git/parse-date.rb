bbabab7f4cdbae95df80ba97b5f6708b9175d558
Arthur Reutenauer <arthur.reutenauer@roh.org.uk>
2016-05-09 14:58:26 +0100

    parse-date.rb: Fixed bug with local time zones.

--- lib/svn2git/parse-date.rb
+++ lib/svn2git/parse-date.rb
@@ -36,7 +36,8 @@ def parse_date(date, author)
   timezone = $timezones[author]
   timezone = 'Europe/Paris' if author == 'Arthur Reutenauer' && time.year < 2011
   ENV['TZ'] = timezone
-  time.iso8601
+  # ENV['TZ'] = 'Europe/Ljubljana'
+  Time.at(time.to_i).iso8601 # This is clearly insane, but works
 end
 
 def test
@@ -48,4 +49,10 @@ end
 
 parse_zonefile(ARGV[2]) if ARGV[2]
 $timezones = { } unless ARGV[2]
+# Last three commits before and including BachoTeX
+# Rewrite bf8006f20cb304ff4380b6df203e444c71ce2968 (1/3)GIT_COMMITTER_DATE=@1460998208 +0100 GIT_COMMITTER_NAME=Arthur Reutenauer, localdate=2016-05-01T00:07:28+02:00
+# Rewrite 2ead57dbb6cb1ad73e4de1cd7c7736039e8f9bca (2/3)GIT_COMMITTER_DATE=@1461073770 +0100 GIT_COMMITTER_NAME=Arthur Reutenauer, localdate=2016-05-01T00:07:28+02:00
+# Rewrite 7ca338ed98b0c37a4dcb5a9884ac52276a584fcf (3/3)GIT_COMMITTER_DATE=@1462054048 +0200 GIT_COMMITTER_NAME=Mojca Miklavec, localdate=2016-05-01T00:07:28+02:00
+# puts '2016-05-01T00:07:28+02:00'
+
 puts parse_date(ARGV.first, ARGV[1])
